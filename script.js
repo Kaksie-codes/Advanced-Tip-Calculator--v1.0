bf56ac9caee181e9721e3c04cf283e543a13623d
Nsikak Akpan <113060075+Kaksie-codes@users.noreply.github.com>
2022-11-10 00:54:31 +0100

    Update  1
    i updated the HTML, CSS and JavaScript Files.

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-let rainy = 1;
-let sunny = -1;
-let overcast = 0;
-const weather = (weather) => {
-if(weather === 1){
-    console.log(`the weather is ${weather}, bring your umbrella`)
-}else if(weather === -1){
-    console.log(`the weather is ${weather}, bring your sunglasses`)
-}else{
-     console.log(`the weather is ${weather}, stay indoors`)
+//DOM Selectors(Variables)
+const billInput = document.getElementById('billTotalInput');
+const tipPercent = document.getElementById('tipInput');
+const increaseBtn = document.getElementById('increaseButton');
+const decreaseBtn = document.getElementById('decreaseButton');
+const totalPeople = document.getElementById('numberOfPeople');
+const perPersonTotal = document.getElementById('perPersonTotal');
+let numPeople = Number(totalPeople.innerText)
+
+//Functions
+const calculateTotalBill = () => {
+    const bill = Number(billInput.value)
+    const tip = (Number(tipPercent.value)/100)*bill;
+    const totalBill = bill + tip;
+    const perPersonBill = totalBill/(numPeople);
+    perPersonTotal.innerText = `$${perPersonBill.toFixed(2)}`;  
 }
+const increasePeople = () => {  
+    numPeople += 1;    
+    console.log(numPeople);
+    totalPeople.innerText = numPeople;   
+    calculateTotalBill();
 }
+const decreasePeople = () => {
+    if(totalPeople.innerText == 1){
+        alert('Invalid Number of People');
+    }else{
+    numPeople -= 1;
+    totalPeople.innerText = numPeople;
+    calculateTotalBill();
+    }    
+}
+
+
+//Event Listeners
+billInput.addEventListener('keyup', calculateTotalBill);
+tipPercent.addEventListener('keyup', calculateTotalBill);
+decreaseBtn.addEventListener('click', decreasePeople);
+increaseBtn.addEventListener('click', increasePeople)
+
